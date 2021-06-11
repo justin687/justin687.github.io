9aaadf9654e914e45ad7f3e7d0f30d383f5a9765
justin687 <84802388+justin687@users.noreply.github.com>
2021-06-10 23:35:06 -0400

    updated styles

--- javascript/main.js
+++ javascript/main.js
@@ -6,14 +6,14 @@ function mobileNav() {
     if(mobileNavActive){
         navMenu.style.width = "0";
         menu.style.position = "absolute";
-        menu.style.color = "white";
+        // menu.style.color = "white";
         menu.classList.replace("fa-times", "fa-bars");
         mobileNavActive = 0;
     }
     else{
         navMenu.style.width = "100%";
         menu.style.position = "fixed";
-        menu.style.color = "black";
+        // menu.style.color = "black";
         menu.classList.replace("fa-bars", "fa-times");
         mobileNavActive = 1;
     }
